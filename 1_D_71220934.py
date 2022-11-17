0bb099bee6b9ce54c764b2eb66fd5ac552a04abe
Yohanes Thathit <yohanes.thathit@ti.ukdw.ac.id>
2022-11-17 16:23:31 +0700

    First

---
+++ 1_D_71220934.py
@@ -0,0 +1,8 @@
+#Kalkulator sederhana yang dapat menghitung luas lingkaran
+print("Kalkulator sederhana yang dapat menghitung luas lingkaran")
+#Test Case 1
+X= input("masukkan jari jari :")
+print(3.14*(4**2))
+#Test Case 2
+Y= input("masukkan jari jari :")
+print(3.14*(15**2))
---
+++ 4_D_71220934.py
@@ -0,0 +1,2 @@
+X=input("Kecepatan tempuh :")
+Y=input("Waktu:")
